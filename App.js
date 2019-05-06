8eff3c3f1e5c4b88cc33ce4f27dc63c34e2a7e43
Cristian de la Sota <cristiandelasota@gmail.com>
2019-05-06 14:52:49 -0300

    commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import HomeScreen from "./HomeScreen";
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("native-base/Fonts/Ionicons.ttf")
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+    return (
+      <HomeScreen />
+    );
+
+  }
+}
